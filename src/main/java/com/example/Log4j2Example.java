7ed92b4416d2cc086e978c28fae00a08dcc25c29
apeiris <mapeiris@hotmail.com>
2024-09-07 08:58:07 -0400

    revised pom.xml for intellij

--- src/main/java/com/example/Log4j2Example.java
+++ src/main/java/com/example/Log4j2Example.java
@@ -5,10 +5,12 @@ public class Log4j2Example {
     private static final Logger logger = LogManager.getLogger(Log4j2Example.class);
     public static void main(String[] args) {
         logger.trace("This is a TRACE message.");
-        logger.debug("This is a DEBUG message.");
+        logger.error("This is a Error message.");
         logger.info("This is an INFO message.");
         logger.warn("This is a WARN message.");
-        logger.error("This is an ERROR message.");
+        logger.debug("this is debug ....");
+
         logger.fatal("This is a FATAL message.");
+        logger.info("this is info too");
     }
 }
